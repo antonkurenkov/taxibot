410e138984f35b2bd939b89641763463c2b60914
antonkurenkov <aka1306@gmail.com>
2019-09-20 15:27:30 +0300

    truing to setup the inner

--- fred.py
+++ fred.py
@@ -1,26 +1,37 @@
-import telebot
+import telepot
 
-bot = telebot.TeleBot('971158672:AAFt3SgFEtM2YEfqOixJeHoslEyleKV3iQY')
+ip = '195.201.137.246'
+port = '1080'
 
+SetProxy = {
+  'https': 'socks5://{}:{}'.format(ip, port)
+}
 
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')
-    elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
-    elif message.text.lower() == 'run':
-        bot.send_message(message.chat.id, 'Считаю...')
-    elif message.text.lower() == 'alt':
-        bot.send_message(message.chat.id, 'Не понимаю..')
+bot = telepot.Bot('971158672:AAFt3SgFEtM2YEfqOixJeHoslEyleKV3iQY')
+bot.getMe()
 
+response = bot.getUpdates()
+print(response)
 
-keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row('Привет', 'Пока')
-
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
-    #bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
-
-bot.polling()
+#
+# @bot.message_handler(content_types=['text'])
+# def send_text(message):
+#     if message.text.lower() == 'привет':
+#         bot.send_message(message.chat.id, 'Привет, мой создатель')
+#     elif message.text.lower() == 'пока':
+#         bot.send_message(message.chat.id, 'Прощай, создатель')
+#     elif message.text.lower() == 'run':
+#         bot.send_message(message.chat.id, 'Считаю...')
+#     elif message.text.lower() == 'alt':
+#         bot.send_message(message.chat.id, 'Не понимаю..')
+#
+#
+# keyboard1 = telebot.types.ReplyKeyboardMarkup()
+# keyboard1.row('Привет', 'Пока')
+#
+# @bot.message_handler(commands=['start'])
+# def start_message(message):
+#     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+#     #bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+#
+# bot.polling()
