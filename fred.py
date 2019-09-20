3908546112caa98026279338ffc95296f0189335
antonkurenkov <aka1306@gmail.com>
2019-09-20 16:28:50 +0300

    truing to setup the inner

--- fred.py
+++ fred.py
@@ -6,7 +6,7 @@ from telebot import types
 bot = telebot.TeleBot('987669302:AAGnJdElKiBTK1Ju81pX9mtprSQ4XddT7IU')
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(func=lambda message: True, commands=['start'])
 def start_message(message):
     markup = types.ReplyKeyboardMarkup()
     markup.row('a', 'v')
@@ -14,7 +14,7 @@ def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=markup)
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(func=lambda message: True, content_types=['text'])
 def send_text(message):
     if message.text.lower() == 'привет':
         bot.send_message(message.chat.id, 'Привет, мой создатель')
