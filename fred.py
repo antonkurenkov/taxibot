51d99d33042ee185c4643e23fcd42c47a3592c35
antonkurenkov <aka1306@gmail.com>
2019-09-20 16:11:22 +0300

    truing to setup the inner

--- fred.py
+++ fred.py
@@ -1,30 +1,24 @@
-import telepot
+import telebot
 
-bot = telepot.Bot('971158672:AAFt3SgFEtM2YEfqOixJeHoslEyleKV3iQY')
-bot.getMe()
+bot = telebot.TeleBot('987669302:AAGnJdElKiBTK1Ju81pX9mtprSQ4XddT7IU')
 
-response = bot.getUpdates()
-print(response)
+bot.polling(none_stop=True)
 
-#
-# @bot.message_handler(content_types=['text'])
-# def send_text(message):
-#     if message.text.lower() == 'привет':
-#         bot.send_message(message.chat.id, 'Привет, мой создатель')
-#     elif message.text.lower() == 'пока':
-#         bot.send_message(message.chat.id, 'Прощай, создатель')
-#     elif message.text.lower() == 'run':
-#         bot.send_message(message.chat.id, 'Считаю...')
-#     elif message.text.lower() == 'alt':
-#         bot.send_message(message.chat.id, 'Не понимаю..')
-#
-#
-# keyboard1 = telebot.types.ReplyKeyboardMarkup()
-# keyboard1.row('Привет', 'Пока')
-#
-# @bot.message_handler(commands=['start'])
-# def start_message(message):
-#     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
-#     #bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
-#
-# bot.polling()
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+keyboard1.row('button1', 'button2')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+    elif message.text.lower() == 'run':
+        bot.send_message(message.chat.id, 'Считаю...')
+    else:
+        bot.send_message(message.chat.id, 'Не понимаю..')
