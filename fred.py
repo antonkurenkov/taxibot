84dad83d4017cb293cb97ab0805d9392cfe7f04d
antonkurenkov <aka1306@gmail.com>
2019-09-20 17:05:32 +0300

    truing to setup the inner

--- fred.py
+++ fred.py
@@ -1,7 +1,19 @@
 import telebot
 from telebot import types
+import time
+import cherrypy
 #from telebot import apihelper
 #apihelper.proxy = {'https':'socks5://orbtl.s5.opennetwork.cc:999', 417554679: 'ybSXViq3'}
+
+WEBHOOK_HOST = '52.57.232.2'
+WEBHOOK_PORT = 443
+WEBHOOK_LISTEN = '52.57.232.2'
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % ('971158672:AAFt3SgFEtM2YEfqOixJeHoslEyleKV3iQY')
 
 bot = telebot.TeleBot('971158672:AAFt3SgFEtM2YEfqOixJeHoslEyleKV3iQY')
 
@@ -17,22 +29,29 @@ def start_message(message):
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def send_text(message):
     if message.text.lower() == 'привет':
+        time.sleep(2)
         bot.send_message(message.chat.id, 'Привет!')
     elif message.text.lower() == 'пока':
+        time.sleep(2)
         bot.send_message(message.chat.id, 'Прощай!')
     elif message.text.lower() == 'input':
+        time.sleep(2)
         bot.send_message(message.chat.id, 'input_method()')
     elif message.text.lower() == 'output':
+        time.sleep(2)
         bot.send_message(message.chat.id, 'output_method()')
     elif message.text.lower() == 'stats':
+        time.sleep(2)
         bot.send_message(message.chat.id, 'stats()')
+    elif message.text.lower() == 'terms':
+        time.sleep(2)
+        bot.send_message(message.chat.id, 'terms()')
     elif message.text.lower() == 'chat':
+        time.sleep(2)
         bot.send_message(message.chat.id, 'chat()')
     else:
-        bot.send_message(message.chat.id, 'Не понимаю..')
+        bot.send_message(message.chat.id, 'Не понимаю...')
 
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
-
-#111
