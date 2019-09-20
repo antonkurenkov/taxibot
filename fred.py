946a2795724dae31dfddd70359d277867b12e443
antonkurenkov <aka1306@gmail.com>
2019-09-20 16:27:08 +0300

    truing to setup the inner

--- fred.py
+++ fred.py
@@ -1,15 +1,18 @@
 import telebot
+from telebot import types
+#from telebot import apihelper
+#apihelper.proxy = {'https':'socks5://orbtl.s5.opennetwork.cc:999', 417554679: 'ybSXViq3'}
 
 bot = telebot.TeleBot('987669302:AAGnJdElKiBTK1Ju81pX9mtprSQ4XddT7IU')
 
 
-
-keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row('button1', 'button2')
-
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+    markup = types.ReplyKeyboardMarkup()
+    markup.row('a', 'v')
+    markup.row('c', 'd', 'e')
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=markup)
+
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
@@ -21,5 +24,6 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Считаю...')
     else:
         bot.send_message(message.chat.id, 'Не понимаю..')
+
 
 bot.polling()
