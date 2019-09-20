d754b1719918f1f1bb41daa9dfe5b6700d278b10
antonkurenkov <aka1306@gmail.com>
2019-09-20 16:48:30 +0300

    truing to setup the inner

--- fred.py
+++ fred.py
@@ -9,25 +9,28 @@ bot = telebot.TeleBot('971158672:AAFt3SgFEtM2YEfqOixJeHoslEyleKV3iQY')
 @bot.message_handler(func=lambda message: True, commands=['start'])
 def start_message(message):
     markup = types.ReplyKeyboardMarkup()
-    markup.row('aaa123', 'vvv456')
-    markup.row('c', 'd', 'e')
+    markup.row('input', 'output')
+    markup.row('stats', 'terms', 'chat')
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=markup)
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def send_text(message):
     if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')
+        bot.send_message(message.chat.id, 'Привет!')
     elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
-    elif message.text.lower() == 'run':
-        bot.send_message(message.chat.id, 'Считаю...')
-    elif message.text.lower() == 'aaa123':
-        bot.send_message(message.chat.id, 'aaa')
-    elif message.text.lower() == 'vvv456':
-        bot.send_message(message.chat.id, 'vvv')
+        bot.send_message(message.chat.id, 'Прощай!')
+    elif message.text.lower() == 'input':
+        bot.send_message(message.chat.id, 'input_method()')
+    elif message.text.lower() == 'output':
+        bot.send_message(message.chat.id, 'output_method()')
+    elif message.text.lower() == 'stats':
+        bot.send_message(message.chat.id, 'stats()')
+    elif message.text.lower() == 'chat':
+        bot.send_message(message.chat.id, 'chat()')
     else:
         bot.send_message(message.chat.id, 'Не понимаю..')
 
 
-bot.polling()
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
