368c79d3e320289f7fafa9cbedcd5f5866cdb347
antonkurenkov <aka1306@gmail.com>
2019-09-20 13:46:01 +0300

    while was trying to add taxovichkov, five-millions crashed it's website again (as usually)

---
+++ fred.py
@@ -0,0 +1,28 @@
+import telebot
+
+bot = telebot.TeleBot('971158672:AAFt3SgFEtM2YEfqOixJeHoslEyleKV3iQY')
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+    elif message.text.lower() == 'run':
+        bot.send_message(message.chat.id, 'Считаю...')
+    else:
+        bot.send_message(message.chat.id, 'Не понимаю..')
+
+
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+keyboard1.row('button1', 'button2')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
